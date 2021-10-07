c2ec30845bca18c76371c8761b7001ce6e94b132
MINYOUNG LEE <>
2021-10-07 16:48:49 +0900

    이벤트 핸들러 추가하기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 function Header(props){
+  function aHandler(){
+    props.onSelect();
+  }
   return(
       <header>
-        <h1><a href="index.html">{props.title}</a></h1>
+        <h1>
+          <a href="index.html" onClick={aHandler}>
+            {/* 리액트에서는 이벤트를 설정할때 prop의 값으로 함수를 줘야한다. */}
+            {props.title}
+          </a>
+        </h1>
       </header>
   )
 }
 function Nav(props){
-  // let lis = [
-  //   <li><a href="1.html">html</a></li>,
-  //   <li><a href="2.html">css</a></li>,
-  //   <li><a href="3.html">js</a></li>
-  // ]
   let lis = [];
   for (let i = 0; i < props.data.length; i++){
     let item = props.data[i];
@@ -44,13 +47,15 @@ function App() {
   let topics = [
     {id:1, title:'html', body:'html is...'},
     {id:2, title:'css', body:'css is...'}
-  ]
+  ];
+  function onSelectHandler(){
+    alert('Hi');
+  }
   return (
     <div>
-      <Header title="WEB"></Header>
+      <Header title="WEB" onSelect={onSelectHandler}></Header>
       <Nav data={topics}></Nav>
       <Article title="html" body="HTML is..."></Article>
-      <Article title="css" body="CSS is..."></Article>
     </div>
   );
 }
